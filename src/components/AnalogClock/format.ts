dad6ba3fb1ca7661dc5ecc71da88dda00ec23114
Yamada Koji <kou64yama@gmail.com>
2021-09-04 13:50:50 +0900

    Fix chronograph format (#16)
    * :bug: Fix chronograph milli second format
    
    * :white_check_mark: Add tests of chronograph

--- src/components/AnalogClock/format.ts
+++ src/components/AnalogClock/format.ts
@@ -1,15 +1,19 @@
-import { HOUR, MINUTE, SECOND } from './utils';
+import { HOUR, MILLISECOND, MINUTE, SECOND } from './utils';
 
 const hours = (duration: number): string =>
   Math.floor(duration / HOUR)
     .toString()
     .padStart(2, '0');
 const minutes = (duration: number): string =>
-  (Math.floor(duration / MINUTE) % 60).toString().padStart(2, '0');
+  (Math.floor(duration / MINUTE) % (HOUR / MINUTE)).toString().padStart(2, '0');
 const seconds = (duration: number): string =>
-  (Math.floor(duration / SECOND) % 60).toString().padStart(2, '0');
+  (Math.floor(duration / SECOND) % (MINUTE / SECOND))
+    .toString()
+    .padStart(2, '0');
 const millis = (duration: number): string =>
-  (Math.floor(duration) % 1000).toString().padEnd(3, '0');
+  (Math.floor(duration / MILLISECOND) % (SECOND / MILLISECOND))
+    .toString()
+    .padStart(3, '0');
 
 export const chronograph = (duration: number): string =>
   `${hours(duration)}°${minutes(duration)}'${seconds(duration)}.${millis(
