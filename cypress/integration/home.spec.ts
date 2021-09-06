704ceb91d743290b2d97862c1a1909ad21d4daf7
Yamada Koji <kou64yama@gmail.com>
2021-09-06 09:26:36 +0900

    :white_check_mark: Add chronograph test

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -4,5 +4,12 @@ describe('home', () => {
     cy.visit('/');
     for (let i = 0; i < 12; i++) cy.tick(318666);
   });
+
+  it('should render the elapsed time correctly', () => {
+    cy.clock(0);
+    cy.visit('/');
+    cy.get('svg > g').click();
+    for (let i = 0; i < 12; i++) cy.tick(5214);
+    cy.get('svg > g').click();
   });
 });
