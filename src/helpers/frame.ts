b80d0b509262e5383614836c9befd9d8dd1175b2
Yamada Koji <kou64yama@gmail.com>
2021-09-06 22:30:54 +0900

    :sparkles: Improve nextFrame (#18)

--- src/helpers/frame.ts
+++ src/helpers/frame.ts
@@ -1,23 +1,31 @@
-export class AbortError extends Error {}
+const ABORT_ERROR_NAME = 'AbortError';
 
-export interface Options {
+const error = () => {
+  const error = new Error(
+    'Failed to wait next animation frame: The user aborted a animation frame',
+  );
+  error.name = ABORT_ERROR_NAME;
+  return error;
+};
+
+export interface FrameOptions {
   signal?: AbortSignal;
 }
 
-export const nextFrame = async (options: Options = {}): Promise<number> =>
+export const nextFrame = async (options: FrameOptions = {}): Promise<number> =>
   new Promise<number>((resolve, reject) => {
     const { signal } = options;
-    const abort = () => {
-      cancelAnimationFrame(handle);
-      reject(
-        new AbortError(
-          'Failed to wait next animation frame: The user aborted a request',
-        ),
-      );
-    };
-    const handle = requestAnimationFrame((time) => {
+    if (signal?.aborted) return reject(error());
+
+    const onAnimationFrame = (time: number) => {
+      signal?.removeEventListener('abort', onAbort);
       resolve(time);
-      signal?.removeEventListener('abort', abort);
-    });
-    signal?.addEventListener('abort', abort);
+    };
+    const onAbort = () => {
+      signal?.removeEventListener('abort', onAbort);
+      cancelAnimationFrame(handle);
+      reject(error());
+    };
+    const handle = requestAnimationFrame(onAnimationFrame);
+    signal?.addEventListener('abort', onAbort);
   });
