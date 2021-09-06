f3a2e49c48a30cd46b7d7cffa318bf7c3712c435
Yamada Koji <kou64yama@gmail.com>
2021-09-06 09:26:18 +0900

    :white_check_mark: Add clock tick test

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,6 +1,8 @@
 describe('home', () => {
-  it('should render correctly', () => {
+  it('should render the current time correctly', () => {
     cy.clock(0);
     cy.visit('/');
+    for (let i = 0; i < 12; i++) cy.tick(318666);
+  });
   });
 });
